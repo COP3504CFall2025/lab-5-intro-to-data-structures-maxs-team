b6fc660989a98d26ab96fba59bec703dcc2dd1c2
Max <mfitzgerald4@ufl.edu>
2025-11-06 14:52:09 -0500

    node struct; behaviors, accessors, insertion, and removal for linkedlist class

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,8 +1,7 @@
 #pragma once
 #include <iostream>
+#include "Node.hpp"
 using namespace std;
-
-
 
 template <typename T>
 class LinkedList {
@@ -13,10 +12,10 @@ public:
 
 	// Accessors
 	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+	Node<T>* getHead();
+	const Node<T>* getHead() const;
+	Node<T>* getTail();
+	const Node<T>* getTail() const;
 
 	// Insertion
 	void addHead(const T& data);
@@ -32,17 +31,132 @@ public:
 	LinkedList<T>& operator=(const LinkedList<T>& rhs);
 
 	// Construction/Destruction
-	LinkedList();
+	LinkedList() = default;
 	LinkedList(const LinkedList<T>& list);
 	LinkedList(LinkedList<T>&& other) noexcept;
 	~LinkedList();
 
 private:
 	// Stores pointers to first and last nodes and count
-	Node* head;
-	Node* tail;
-	unsigned int count;
+	Node<T>* head = nullptr;
+	Node<T>* tail = nullptr;
+	unsigned int count = 0;
 
 };
 
+template<typename T>
+[[nodiscard]] unsigned int LinkedList<T>::getCount() const {
+	return count;
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getHead() {
+	return head;
+}
+
+template<typename T>
+const Node<T>* LinkedList<T>::getHead() const {
+	return head;
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getTail() {
+	return tail;
+}
+
+template<typename T>
+const Node<T>* LinkedList<T>::getTail() const {
+	return tail;
+}
+
+template <typename T>
+void LinkedList<T>::addHead(const T& data) {
+	if (head != nullptr) {
+		head -> prev = new Node(data, nullptr, &head);
+		head = &(head -> prev);
+	} else {
+		head = new Node(data, nullptr, nullptr);
+		tail = &head;
+	}
+	count++;
+}
+
+template <typename T>
+void LinkedList<T>::addTail(const T& data) {
+	if (tail != nullptr) {
+		tail -> next = new Node(data, &tail, nullptr);
+		tail = &(tail -> next);
+	} else {
+		tail = new Node(data, nullptr, nullptr);
+		head = &tail;
+	}
+	count++;
+}
+
+template <typename T>
+bool LinkedList<T>::removeHead() {
+	if (head == nullptr)
+		return false;
+	if ((head -> next) != nullptr) {
+		head = &(head -> next);
+		delete head -> prev;
+		head -> prev = nullptr;
+	} else {
+		delete head;
+		head = nullptr;
+	}
+	count--;
+	return true;
+}
+
+template <typename T>
+bool LinkedList<T>::removeTail() {
+	if (tail == nullptr)
+		return false;
+	if ((tail -> prev) != nullptr) {
+		tail = &(tail -> prev);
+		delete tail -> next;
+		tail -> next = nullptr;
+	} else {
+		delete tail;
+		tail = nullptr;
+	}
+	count--;
+	return true;
+}
+
+template <typename T>
+void LinkedList<T>::Clear() {
+	if (head != nullptr) {
+		Node<T>* curr = &head;
+		while (curr -> next != nullptr) {
+			curr = &(curr -> next);
+			delete curr -> prev;
+			curr -> prev = nullptr;
+		}
+		delete curr;
+		curr = nullptr;
+		head = nullptr;
+		tail = nullptr;
+		count = 0;
+	}
+}
+
+template <typename T>
+void LinkedList<T>::printForward() const {
+	Node<T>* curr = &head;
+	while (curr != nullptr) {
+		std::cout << curr -> data << std::endl;
+		curr = &(curr -> next);
+	}
+}
+
+template <typename T>
+void LinkedList<T>::printReverse() const {
+	Node<T>* curr = &tail;
+	while (curr != nullptr) {
+		std::cout << curr -> data << std::endl;
+		curr = &(curr -> prev);
+	}
+}
 
---
+++ Node.hpp
@@ -0,0 +1,11 @@
+#pragma once
+template<typename T>
+struct Node {
+    T data;
+    Node* prev;
+    Node* next;
+
+    Node() = default;
+    Node(const T& d) : data(d) {}
+    Node(const T& d, Node* p, Node* n) : data(d), prev(p), next(n) {}
+};
