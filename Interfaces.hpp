f949d64b06787fe016ed81e661e026e7b54f048b
Max <mfitzgerald4@ufl.edu>
2025-11-07 09:11:44 -0500

    interfaces

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -6,18 +6,33 @@
 
 template <typename T>
 class StackInterface {
-
+    void push(const T& item);
+    T pop();
+    T peek() const;
+    [[nodiscard]] std::size_t getSize() const noexcept;
+    virtual ~StackInterface() = 0;
 };
 
 
 template <typename T>
 class QueueInterface {
-
+    void enqueue(const T& item);
+    T dequeue();
+    T peek() const;
+    std::size_t getSize() const noexcept;
+    virtual ~QueueInterface() = 0;
 };
 
 
 template <typename T>
 class DequeInterface {
-
+    void pushFront(const T& item);
+    void pushBack(const T& item);
+    T popFront();
+    T popBack();
+    const T& front() const;
+    const T& back() const;
+    std::size_t getSize() const noexcept;
+    virtual ~DequeInterface() = 0;
 };
 
