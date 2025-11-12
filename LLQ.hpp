75940e1067b854ea2165c2174ef5c71fd090da3e
Max <mfitzgerald4@ufl.edu>
2025-11-12 01:38:55 -0500

    llq

--- LLQ.hpp
+++ LLQ.hpp
@@ -26,3 +26,34 @@ public:
     std::size_t getSize() const noexcept override;
 
 };
+
+
+template<typename T>
+LLQ<T>::LLQ() : list(LinkedList<T>()) {}
+
+template<typename T>
+void LLQ<T>::enqueue(const T& item) {
+    list.addHead(item);
+}
+
+template<typename T>
+T LLQ<T>::dequeue() {
+    if (list.getCount() == 0)
+        throw std::runtime_error("no elements");
+    T temp = list.getTail() -> data;
+    list.removeTail();
+    return temp;
+}
+
+template<typename T>
+T LLQ<T>::peek() const {
+    if (list.getCount() == 0)
+        throw std::runtime_error("no elements");
+    T temp = list.getTail() -> data;
+    return temp;
+}
+
+template<typename T>
+std::size_t LLQ<T>::getSize() const noexcept {
+    return list.getCount();
+}
